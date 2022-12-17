090ed7a132d4e2889814a1e9efd66ea328ff42be
Joe Hinkle <joehinkle11@gmail.com>
2022-12-17 01:30:16 -0600

    boxing tests

--- tests/primitive_wrappers.ts
+++ tests/primitive_wrappers.ts
@@ -1,6 +1,6 @@
 import {describe} from '@jest/globals';
 import { testSafeEvalAgainstNative } from './helpers';
 
-describe('autoboxing tests', () => {
-  testSafeEvalAgainstNative("({[1]:true})");
+describe('number wrapper tests', () => {
+  testSafeEvalAgainstNative("new Number(1)");
 });
