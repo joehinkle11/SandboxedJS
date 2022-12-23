9b69ef0be1c44d27b65382c4c1ff07815cc897d5
Joe Hinkle <joehinkle11@gmail.com>
2022-12-22 20:08:59 -0600

    sfunction

--- bindings/src/CodeGen/NativeTypeToSType.ts
+++ bindings/src/CodeGen/NativeTypeToSType.ts
@@ -10,10 +10,17 @@ export function nativeTypeToSType(
   } else if (nativeType.isBoolean()) {
     return "SBooleanValue<any, number>";
   } else if (nativeType.isObject()) {
-    return "SObjectValue<any, any, any>";
+    const isCallable = nativeType.getCallSignatures().length > 0;
+    const isConstructor = nativeType.getConstructSignatures().length > 0;
+    // todo: we need to make a distinction between callables and constructables
+    if (isCallable || isConstructor) {
+      return "SFunction<any>"
+    } else {
+      return "SObjectValue<any, any, any>";
+    }
   } else {
     throw new Error("Todo nativeTypeToSType for: " + nativeType.getText())
   }
 }
 
-export type STypeString = "SBooleanValue<any, number>" | "SNumberValue<any, number>" | "SObjectValue<any, any, any>";
+export type STypeString = "SBooleanValue<any, number>" | "SNumberValue<any, number>" | "SObjectValue<any, any, any>" | "SFunction<any>";
