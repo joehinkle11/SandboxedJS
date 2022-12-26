d29f603f74484af907974adf1543f805a5203d7e
Joe Hinkle <joehinkle11@gmail.com>
2022-12-26 11:36:41 -0600

    fixed boolean missing auto-generated toString bindings

--- bindings/src/Index.ts
+++ bindings/src/Index.ts
@@ -14,13 +14,27 @@ const target = "lib.es2022.d.ts";
 // const target = "lib.d.ts";
 // const target = "lib.es5.d.ts";
 
+// It seems some of typescript's default definitions missed some things, add them here to get auto-generated bindings.
+const extraHardcodedTypeDefs = `
+declare global {
+  interface Boolean {
+    toString(): string;
+  }
+}
+interface Boolean {
+  toString(): string;
+}
+`;
+
 const filesToDoWorkOn = importTSLibFilesRecursively(target, project);
+filesToDoWorkOn.push(project.createSourceFile("extra_hardcoded_type_defs.d.ts", extraHardcodedTypeDefs));
+
 
 let builtInBindingStore: BuiltInBindingStore = new BuiltInBindingStore();
 collectInterfaces(filesToDoWorkOn, builtInBindingStore);
 collectVariables(filesToDoWorkOn, builtInBindingStore);
 
-exportToFile(builtInBindingStore);
+exportToFile(builtInBindingStore, extraHardcodedTypeDefs);
 
 /*
 function addFileGlobalVariables(filePath: string) {
